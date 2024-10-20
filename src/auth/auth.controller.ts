d1b1e9b676969f8721816465dd65da98ddc56b19
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-20 20:48:59 +0700

    Refactor AuthController to use LocalAuthGuard for login endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LoggedInDto } from './dto/logged-in.dto';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
+  @UseGuards(LocalAuthGuard)
   @Post('login')
-  login(@Request() request: any) {
-    const access_token = 'FAKE_TOKEN'
+  login(@Request() request: { user: LoggedInDto }) {
+    const access_token = this.authService.login(request.user)
     return { access_token };
   }
 }
