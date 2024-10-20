a32a28569ab0854078ef8d16f8963e8fbcf25825
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-20 20:06:02 +0700

    complete user module

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { IsEnum, IsNotEmpty, IsString } from "class-validator";
+import { Role, User } from "../entities/user.entity";
+
+export class CreateUserDto implements Partial<User> {
+    @IsString()
+    @IsNotEmpty()
+    username: string;
+
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+
+    @IsEnum(Role)
+    role: Role;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
+
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>
+  ) { }
+
+  async create(createUserDto: CreateUserDto) {
+
+    // gen salt
+    const salt = await bcrypt.genSalt();
+
+    // enc password
+    const encPassword = await bcrypt.hash(createUserDto.password, salt)
+
+    // replace password with encPassword
+    const userWithEncPassword = { ...createUserDto, password: encPassword };
+
+    // saved
+    const savedUser = await this.userRepository.save(userWithEncPassword);
+
+    // spread password filed from savedUser
+    const { password, ...userWithoutPassword } = savedUser;
+
+    // return user without password filed
+    return userWithoutPassword;
+
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.userRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.userRepository.findOneBy({ id: id });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    console.log(updateUserDto);
+    // if password is provided
+    if (updateUserDto.password) {
+      // gen salt
+      const salt = await bcrypt.genSalt();
+
+      // enc password
+      const encPassword = await bcrypt.hash(updateUserDto.password, salt);
+
+      // replace password with encPassword
+      updateUserDto = { ...updateUserDto, password: encPassword };
+    }
+    return this.userRepository.update(id, updateUserDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    return this.userRepository.delete(id);
   }
 }
