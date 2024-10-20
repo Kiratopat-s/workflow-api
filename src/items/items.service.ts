6934e688dd0165a69f711951865677a05a117a65
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-20 21:30:37 +0700

    Refactor ItemsService to include delete and approve functionality

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Item } from './entities/item.entity';
+import { Item, ItemStatus } from './entities/item.entity';
 import { Repository } from 'typeorm';
 
 
@@ -76,13 +76,48 @@ export class ItemsService {
   }
 
   /**
-   * Description
+   * Delete item by id in database or throw NotFoundException if not found id in database
    * @author Kiratipat
    * @date 2024-10-20
-   * @param {any} id:number
-   * @returns {DeleteResult}
+   * @param {number} id
+   * @returns {Item}
    */
-  remove(id: number) {
-    return this.itemRepository.delete(id);
+  async remove(id: number) {
+    const item = await this.itemRepository.findOneBy({ id })
+    if (!item) {
+      throw new NotFoundException(`Not found: id=${id}`)
+    }
+
+    return this.itemRepository.delete({ id })
+  }
+
+  /**
+   * Approve item by id in database
+   * Or throw NotFoundException if not found id in database
+   * Or id is empty, item is not found in database, item is already approved in database
+   * @author Kiratipat
+   * @date 2024-10-20
+   * @param {number} id
+   * @returns {Item}
+   */
+  async approve(id: number) {
+    // id should not empty
+    if (!id) {
+      throw new NotFoundException(`id should not empty`)
+    }
+
+    // item should found
+    const item = await this.itemRepository.findOneBy({ id })
+    if (!item) {
+      throw new NotFoundException(`not found: id={${id}}`)
+    }
+
+    // prepare items
+    // const approveItem = {...item, status: ItemStatus.APPROVED}
+    // return await this.itemRepository.save(approveItem)
+
+    item.status = ItemStatus.APPROVED
+
+    return await this.itemRepository.save(item)
   }
 }
