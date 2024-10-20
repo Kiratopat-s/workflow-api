72ac45232b8180ce1e0e17eda432833fa4f3d055
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-20 18:24:21 +0700

    complete item module

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,1 +1,21 @@
-export class CreateItemDto {}
+import { IsNotEmpty, isNumber, IsNumber, IsOptional, IsString } from "class-validator";
+import { Item } from "../entities/item.entity";
+
+export class CreateItemDto implements Partial<Item> {
+
+    @IsString()
+    @IsNotEmpty()
+    title: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    amount: number;
+
+    @IsNumber()
+    @IsNotEmpty()
+    price: number;
+
+    @IsOptional()
+    contactMobileNo: string;
+
+}
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,28 +1,88 @@
 import { Injectable } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Item } from './entities/item.entity';
+import { Repository } from 'typeorm';
+
 
 @Injectable()
 export class ItemsService {
+
+  constructor(
+    @InjectRepository(Item) private itemRepository: Repository<Item>
+  ) { }
+
+  /**
+   * Insert new item to database
+   * @author Kiratipat
+   * @date 2024-10-20
+   * @param {CreateItemDto} createItemDto
+   * @returns {Item}
+   */
   create(createItemDto: CreateItemDto) {
-    // ### debug
-    console.log('createItemDto', createItemDto);
-    return 'This action adds a new item';
+    return this.itemRepository.save(createItemDto);
   }
 
+  /**
+   * List all items in database DESC order by id
+   * @author Kiratipat
+   * @date 2024-10-20
+   * @returns {Item[]}
+   */
   findAll() {
-    return `This action returns all items`;
+    return this.itemRepository.find({
+      order: {
+        id: 'DESC'
+      }
+    });
   }
 
+  /**
+   * Find one item by id in database
+   * @author Kiratipat
+   * @date 2024-10-20
+   * @param {number} id
+   * @returns {Item}
+   */
   findOne(id: number) {
-    return `This action returns a #${id} item`;
+    return this.itemRepository.findOneBy({ id });
   }
 
+  // /**
+  //  * Update item by id in database
+  //  * @author Kiratipat
+  //  * @date 2024-10-20
+  //  * @param {any} id:number
+  //  * @param {any} updateItemDto:UpdateItemDto
+  //  * @returns {UpdateResult}
+  //  */
+  // update(id: number, updateItemDto: UpdateItemDto) {
+  //   return this.itemRepository.update(id, updateItemDto);
+  // }
+
+  /**
+   * Update item by id in database
+   * @author Kiratipat
+   * @date 2024-10-20
+   * @param {number} id
+   * @param {UpdateItemDto} updateItemDto
+   * @returns {id, UpdateItemDto}
+   */
   update(id: number, updateItemDto: UpdateItemDto) {
-    return `This action updates a #${id} item`;
+    return this.itemRepository.save({
+      id, ...updateItemDto
+    })
   }
 
+  /**
+   * Description
+   * @author Kiratipat
+   * @date 2024-10-20
+   * @param {any} id:number
+   * @returns {DeleteResult}
+   */
   remove(id: number) {
-    return `This action removes a #${id} item`;
+    return this.itemRepository.delete(id);
   }
 }
