868c343684c4cfaba6bb44953456bd83eb7cd0ec
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-21 10:05:52 +0700

    Refactor MsgParseIntPipe to extend ParseIntPipe and include custom error handling

--- src/pipes/msg-parse-int.pipe.ts
+++ src/pipes/msg-parse-int.pipe.ts
@@ -1,8 +1,21 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { ArgumentMetadata, BadRequestException, Injectable, ParseIntPipe } from '@nestjs/common';
 
 @Injectable()
-export class MsgParseIntPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
-    return value;
+export class MsgParseIntPipe extends ParseIntPipe {
+
+  paramName = ''
+
+  constructor(private msg = 'ควรเป็นตัวเลข') {
+    super()
   }
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    const { data } = metadata;
+    this.paramName = data;
+    return super.transform(value, metadata);
+  }
+
+  exceptionFactory = (error: any) => {
+    return new BadRequestException(`${this.paramName} ${this.msg}`);
+  };
 }
