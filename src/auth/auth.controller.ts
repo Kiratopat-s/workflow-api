4316dc398092437f9fd5e970ae47ea7b5a81b3f4
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-21 11:07:19 +0700

    Refactor AuthController to use PerfLoggerInterceptor for logging performance metrics

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards, UseInterceptors } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { LoggedInDto } from './dto/logged-in.dto';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { LoggedInDto } from './dto/logged-in.dto';
 import { RefreshJwtAuthGuard } from './guards/refresh-jwt-auth.guard';
+import { PerfLoggerInterceptor } from 'src/interceptors/perf-logger.interceptor';
 
+@UseInterceptors(PerfLoggerInterceptor)
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
@@ -25,5 +27,3 @@ export class AuthController {
     return this.authService.refreshToken(request.user);
   }
 }
-
-
