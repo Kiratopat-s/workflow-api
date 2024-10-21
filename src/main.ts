c753c071de0e744d904b701252995d6425aaacfa
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-21 10:04:20 +0700

    Refactor main.ts to include global validation pipe and query failed error filter

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { QueryFailedErrorFilter } from './filters/query-failed-error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const { httpAdapter } = app.get(HttpAdapterHost)
+
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new QueryFailedErrorFilter(httpAdapter))
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
 
