cc178f68cb9dda6498cd80ff961ac5f6ef5e2e70
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-21 11:27:04 +0700

    Refactor BudgetController to add endpoint for retrieving yearly budget information

--- src/budget/budget.controller.ts
+++ src/budget/budget.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { BudgetService } from './budget.service';
 
 @Controller('budget')
 export class BudgetController {
-  constructor(private readonly budgetService: BudgetService) {}
+  constructor(private readonly budgetService: BudgetService) { }
+
+  @Get(':year')
+  findYearBudger(@Param('year') year: number) {
+    return this.budgetService.findYearBudger(year);
+  }
+
 }
--- src/budget/budget.service.ts
+++ src/budget/budget.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { YearBudgetDto } from './dto/yearbudget.dto';
+import axios from 'axios';
 
 @Injectable()
-export class BudgetService {}
+export class BudgetService {
+
+    async findYearBudger(year: number): Promise<YearBudgetDto> {
+        const result = await axios.get(`https://year-budget-api.onrender.com/budget/${year}`, {
+            headers: {
+                'X-Auth-Token': 'peaxodds2024'
+            }
+        });
+        return result.data;
+    }
+}
