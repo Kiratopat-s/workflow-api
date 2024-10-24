056ffb80a904abde35b846e2937ccc9ec6c3ff64
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-24 10:18:48 +0700

    Refactor AuthController and AuthService for better code organization and testing

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,37 +1,80 @@
-import { Controller, Get, Post, Request, UseGuards, UseInterceptors } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { LocalAuthGuard } from './guards/local-auth.guard';
+import { Injectable, Logger } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { UsersService } from '../users/users.service';
 import { LoggedInDto } from './dto/logged-in.dto';
-import { RefreshJwtAuthGuard } from './guards/refresh-jwt-auth.guard';
-import { PerfLoggerInterceptor } from 'src/interceptors/perf-logger.interceptor';
-import { Oauth2AuthGuard } from './guards/oauth2-auth.guard';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { v7 as uuidv7 } from 'uuid';
 
-@UseInterceptors(PerfLoggerInterceptor)
-@Controller('auth')
-export class AuthController {
-  constructor(private readonly authService: AuthService) { }
+@Injectable()
+export class AuthService {
+  private logger = new Logger(AuthService.name);
 
-  @UseInterceptors(PerfLoggerInterceptor)
-  @UseGuards(LocalAuthGuard)
-  @Post('login')
-  login(@Request() request: { user: LoggedInDto }) {
-    return this.authService.login(request.user);
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+    private configService: ConfigService
+  ) { }
+
+  private async getUserWithoutPassword(username: string): Promise<LoggedInDto | null> {
+    const user = await this.usersService.findOneByUsername(username);
+    if (!user) {
+      this.logger.debug(`User not found: username=${username}`);
+      return null;
+    }
+    const { password, ...userWithoutPassword } = user;
+    return userWithoutPassword;
   }
 
-  @UseGuards(RefreshJwtAuthGuard)
-  @Post('refresh')
-  refreshToken(@Request() request: { user: LoggedInDto }) {
-    return this.authService.refreshToken(request.user);
+  async validateUser(username: string, password: string): Promise<LoggedInDto | null> {
+    const user = await this.usersService.findOneByUsername(username);
+    if (!user) {
+      this.logger.debug(`User not found: username=${username}`);
+      return null;
+    }
+
+    if (await bcrypt.compare(password, user.password)) {
+      const { password, ...userWithoutPassword } = user;
+      return userWithoutPassword;
+    } else {
+      this.logger.debug(`Wrong password: username=${username}`);
+      return null;
+    }
   }
 
-  @Get('login-oauth2-redirect-url')
-  loginOauth2RedirectUrl(): { redirectUrl: string } {
-    return { redirectUrl: this.authService.getOauth2RedirectUrl() };
+  async validateUserByAccessToken(accessToken: string): Promise<LoggedInDto | null> {
+    const userInfo: { preferred_username: string } = await this.jwtService.decode(accessToken) as { preferred_username: string };
+    return this.getUserWithoutPassword(userInfo.preferred_username);
   }
 
-  @UseGuards(Oauth2AuthGuard)
-  @Post('login-oauth2')
-  loginKeycloak(@Request() request: { user: LoggedInDto }) {
-    return this.authService.login(request.user)
+  private generateToken(payload: LoggedInDto, secret: string, expiresIn: string): string {
+    return this.jwtService.sign(payload, { secret, expiresIn });
+  }
+
+  login(loggedInDto: LoggedInDto): { access_token: string; refresh_token: string } {
+    const payload: LoggedInDto = { ...loggedInDto, sub: loggedInDto.id };
+    const access_token = this.jwtService.sign(payload);
+
+    const refreshTokenSecret = this.configService.get<string>('REFRESH_JWT_SECRET');
+    const refreshTokenExpiresIn = this.configService.get<string>('REFRESH_JWT_EXPIRES_IN');
+    const refresh_token = this.generateToken(payload, refreshTokenSecret, refreshTokenExpiresIn);
+
+    return { access_token, refresh_token };
+  }
+
+  refreshToken(loggedInDto: LoggedInDto): { access_token: string } {
+    const payload: LoggedInDto = { ...loggedInDto, sub: loggedInDto.id };
+    const access_token = this.jwtService.sign(payload);
+    return { access_token };
+  }
+
+  getOauth2RedirectUrl(): string {
+    const auth_url = this.configService.get<string>('OAUTH2_AUTH_URL');
+    const client_id = this.configService.get<string>('OAUTH2_CLIENT_ID');
+    const redirect_uri = this.configService.get<string>('OAUTH2_CALLBACK_URL');
+    const scope = encodeURIComponent(this.configService.get<string>('OAUTH2_SCOPE'));
+    const response_type = this.configService.get<string>('OAUTH2_RESPONSE_TYPE');
+    const state = uuidv7();
+    return `${auth_url}?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}&response_type=${response_type}&state=${state}`;
   }
 }
