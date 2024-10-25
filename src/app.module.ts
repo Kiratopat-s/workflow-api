b641fba98468b183fcc60d1dad35dd2d98f023a1
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-25 09:43:32 +0700

    Refactor import paths and add GithubModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
-// app.module.ts
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
 import { ConfigModule } from '@nestjs/config';
 import { DbModule } from './db/db.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import dbConfig from './db/db.config'
+import dbConfig from './db/db.config';
 import { LoginLoggerMiddleware } from './middlewares/login-logger.middleware';
 import { BudgetModule } from './budget/budget.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GithubModule } from './github/github.module';
 
 @Module({
   imports: [
@@ -18,14 +18,15 @@ import { AppService } from './app.service';
     DbModule,
     UsersModule,
     AuthModule,
-    BudgetModule],
+    BudgetModule,
+    GithubModule
+  ],
   controllers: [AppController],
   providers: [AppService],
-
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoginLoggerMiddleware)
-      .forRoutes({ path: '*login*', method: RequestMethod.POST })
+      .forRoutes({ path: '*login*', method: RequestMethod.POST });
   }
 }
